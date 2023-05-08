fbc4a939d5c4bc7def00ffeab449a60a7e075782
YeongT <94744740+YeongT@users.noreply.github.com>
2023-05-08 18:21:25 +0900

    chore: update gradle version

--- app/src/main/java/com/kanduit/localcommunity/ui/main/MainActivity.kt
+++ app/src/main/java/com/kanduit/localcommunity/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kanduit.localcommunity.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.kanduit.localcommunity.R
 
 class MainActivity : AppCompatActivity() {
